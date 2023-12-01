e3a2925806d537e7f769b4326aa2248f52e23f89
Aurélien Girault <me@agirault.com>
2023-12-01 21:56:21 +0100

    :sparkles: Feat(2023): day 1 - puzzle 2

--- 2023/1/trebuchet.test.ts
+++ 2023/1/trebuchet.test.ts
@@ -1,17 +1,57 @@
-import { puzzle1 } from './trebuchet'
+import { puzzle1, puzzle2 } from './trebuchet'
 import readInput from '../../utils/readInput'
 
-const exampleInput = readInput(__dirname, 'puzzle-1.example.input')
+const puzzle1exampleInput = readInput(__dirname, 'puzzle-1.example.input')
+const puzzle2exampleInput = readInput(__dirname, 'puzzle-2.example.input')
 const myInput = readInput(__dirname, 'puzzle-1.my.input')
 
 describe('2023 - 1 - trebuchet', () => {
   test('puzzle 1 with example input', () => {
-    expect(puzzle1(exampleInput)).toBe(142)
+    expect(puzzle1(puzzle1exampleInput)).toBe(142)
   })
 
   test('puzzle 1 with my input', () => {
     expect(puzzle1(myInput)).toBe(47)
   })
+
+  test('puzzle 2 with example input', () => {
+    expect(puzzle2(puzzle2exampleInput)).toBe(281)
+  })
+
+  test('puzzle 2 with only numbers', () => {
+    expect(puzzle2('5173')).toBe(53)
+  })
+
+  test('puzzle 2 with word at the beginning and at the end', () => {
+    expect(puzzle2('six4ornferfi8three')).toBe(63)
+  })
+
+  test('puzzle 2 with word at the beginning', () => {
+    expect(puzzle2('six4ornferfi8')).toBe(68)
+  })
+
+  test('puzzle 2 with word at the end', () => {
+    expect(puzzle2('4ornferfi8three')).toBe(43)
+  })
+
+  test('puzzle 2 with word one', () => {
+    expect(puzzle2('one887')).toBe(17)
+  })
+
+  test('puzzle 2 with word two', () => {
+    expect(puzzle2('4oerfpio77675twonetwo')).toBe(42)
+  })
+
+  test('puzzle 2 with word six', () => {
+    expect(puzzle2('kiuzenfsixzefn984782')).toBe(62)
+  })
+  test('puzzle 2 with word eight', () => {
+    expect(puzzle2('eightnine')).toBe(89)
+  })
+
+  test('seven5seven3seven4zgtnnkkgseveneightwosg is 72', () => {
+    expect(puzzle2('seven5seven3seven4zgtnnkkgseveneightwosg')).toBe(72)
+  })  
 })
 
 
--- 2023/1/trebuchet.ts
+++ 2023/1/trebuchet.ts
@@ -13,4 +13,32 @@ export const puzzle1 = (input: string) => {
   }, 0)
 }
 
+export const puzzle2 = (input: string) => {
+  function alphaToDigit(alpha: string): number {
+    switch (alpha) {
+      case 'one': return 1
+      case 'two': return 2
+      case 'three': return 3
+      case 'four': return 4
+      case 'five': return 5
+      case 'six': return 6
+      case 'seven': return 7
+      case 'eight': return 8
+      case 'nine': return 9
+      default: return alpha as unknown as number
+    }
+  }
+
+  return input.split('\n').reduce((acc, curr) => {
+    if (!curr) return acc
+
+    const regex = /(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))/g
+    const digits = [...curr.matchAll(regex)].map(match => alphaToDigit(match[1]))
+    const calibration = Number(`${digits[0]}${digits[digits.length - 1]}`)
+
+    return acc + calibration
+  }, 0)
+}
+
 console.log(puzzle1(input))
+console.log(puzzle2(input))
